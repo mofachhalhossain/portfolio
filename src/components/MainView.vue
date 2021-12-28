4dc3c7387213b635f3779211d9a47d066a675592
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-12-28 15:26:56 +0600

    small change

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="upper_chunck">
-  <VueWriter :array="arr" :start="2000" :typeSpeed="70" :eraseSpeed="80" :delay="1000"/>
+  <VueWriter :array="arr" :start="1000" :typeSpeed="70" :eraseSpeed="80" :delay="1000"/>
 </div>
 <div class="lower_chunck">
   <label class="name">{{ name }}</label><br><br>
@@ -39,14 +39,14 @@ export default {
   /* text-shadow: 0 0 5px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #ff00de, 0 0 70px #ff00de, 0 0 80px #ff00de, 0 0 100px #ff00de, 0 0 150px #ff00de; */
   color: #fff;
   text-shadow:
+    0 0 2px #fff,
     0 0 5px #fff,
     0 0 10px #fff,
-    0 0 15px #fff,
+    0 0 20px #0ff,
     0 0 30px #0ff,
     0 0 40px #0ff,
     0 0 50px #0ff,
-    0 0 60px #0ff,
-    0 0 70px #0ff;
+    0 0 60px #0ff;
 }
 
 @media screen and (max-width: 768px) {
